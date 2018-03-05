d3fb9fd40e8971edc2f87fb95487cbed89ec2d92
Dan Rosen <drosen84@gmail.com>
2018-03-05 11:43:07 -0600

    completed server layer

--- blogRouter.js
+++ blogRouter.js
@@ -11,3 +11,8 @@ const {BlogPosts} = require('./models');
 BlogPosts.create('My Life as a Snail', 'Life is hard as a snail. You gotta look out for salt...', 'by Dan R');
 BlogPosts.create('Dress for Mediocrity', 'Heres what you do to dress for mediocrity...', 'by Leslie P');
 BlogPosts.create('How to Win Go Fish', 'The biggest mistake people make when playing Go Fish is...', 'by Winnie J');
+
+
+router.get('/', (req, res) => {
+  res.json(BlogPosts.get());
+});
--- server.js
+++ server.js
@@ -9,3 +9,7 @@ const blogRouter = require('./blogRouter');
 app.use(morgan('common'));
 
 app.use('/blog-posts', blogRouter);
+
+app.listen(process.env.PORT || 8080, () => {
+  console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
+});
