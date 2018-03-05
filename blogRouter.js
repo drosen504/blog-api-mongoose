972b45afa0a63a1da947cc2c6b744babfa5be0cc
Dan Rosen <drosen84@gmail.com>
2018-03-05 11:23:32 -0600

    first update. some of the blogRouter and server basics filled in

---
+++ blogRouter.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
+
+const {BlogPosts} = require('./models');
+
+BlogPosts.create('My Life as a Snail', 'Life is hard as a snail. You gotta look out for salt...', 'by Dan R');
+BlogPosts.create('Dress for Mediocrity', 'Heres what you do to dress for mediocrity...', 'by Leslie P');
+BlogPosts.create('How to Win Go Fish', 'The biggest mistake people make when playing Go Fish is...', 'by Winnie J');
---
+++ server.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+const blogRouter = require('./blogRouter');
+
+app.use(morgan('common'));
+
+app.use('/blog-posts', blogRouter);
