08944d38ca1cb2ad2decdcd744fea2430aa4f753
Dan Rosen <drosen84@gmail.com>
2018-03-05 13:28:52 -0600

    successfully implemented put

--- blogRouter.js
+++ blogRouter.js
@@ -32,6 +32,32 @@ router.post('/', jsonParser, (req, res) => {
   res.status(201).json(item);
 });
 
+router.put('/:id', jsonParser, (req, res) => {
+  const requiredFields = ['title', 'content', 'author'];
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`;
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+  if (req.params.id !== req.body.id) {
+    const message = (
+      `Request path id (${req.params.id}) and request body id `
+      `(${req.body.id}) must match`);
+    console.error(message);
+    return res.status(400).send(message);
+  }
+  console.log(`Updating blog post ID \`${req.params.id}\``);
+  const updatedItem = BlogPosts.update({
+    id: req.params.id,
+    title: req.body.title,
+    content: req.body.content,
+    author: req.body.author
+  });
+  res.status(204).end();
+});
 
 
 module.exports = router;
