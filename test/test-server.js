c21e8a2e16f6da03029bf5552aed2bb2c67fee86
Dan Rosen <drosen84@gmail.com>
2018-03-09 10:40:49 -0600

    modified gitignore file. added get endpoint tests

---
+++ test/test-server.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+const expect = chai.expect;
+
+const {app, runServer, closeServer} = require('../server');
+
+chai.use(chaiHttp);
+
+
+describe('Blog Posts', function() {
+
+  before(function() {
+    return runServer();
+  });
+
+  after(function() {
+    return closeServer();
+  });
+
+  it('should list items on GET'), function() {
+    return chai.request(app)
+      .get('/shopping-list')
+      .then(function(res) {
+        expect(res).to.have.status(200);
+        expect(res).to.be.json;
+        expect(res.body).to.be.a('array');
+        expect(res.body.length).to.be.at.least(1);
+        const expectedKeys = ['id', 'title', 'content', 'author'];
+        res.body.forEach(function(item) {
+          expect(item).to.be.a('object');
+          expect(item).to.include.keys(expectedKeys);
+        });  
+      });
+  };
+
+  
+});
