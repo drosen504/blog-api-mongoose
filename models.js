01fd0154e1573f40c5a05e3834c41e5db52f09c4
Dan Rosen <drosen84@gmail.com>
2018-03-14 10:34:14 -0500

    updated server and model.js files to work with mongo/mongoose

--- models.js
+++ models.js
@@ -1,72 +1,57 @@
 'use strict';
 
-const uuid = require('uuid');
+const mongoose = require('mongoose');
 
-// This module provides volatile storage, using a `BlogPost`
-// model. We haven't learned about databases yet, so for now
-// we're using in-memory storage. This means each time the app stops, our storage
-// gets erased.
+// this is our schema to represent a restaurant
+const postSchema = mongoose.Schema({
+  name: {type: String, required: true},
+  borough: {type: String, required: true},
+  cuisine: {type: String, required: true},
+  address: {
+    building: String,
+    coord: [String],
+    street: String,
+    zipcode: String
+  },
+  // grades will be an array of objects
+  grades: [{
+    date: Date,
+    grade: String,
+    score: Number
+  }]
+});
 
-// Don't worry too much about how BlogPost is implemented.
-// Our concern in this example is with how the API layer
-// is implemented, and getting it to use an existing model.
+// *virtuals* (http://mongoosejs.com/docs/guide.html#virtuals)
+// allow us to define properties on our object that manipulate
+// properties that are stored in the database. Here we use it
+// to generate a human readable string based on the address object
+// we're storing in Mongo.
+postSchema.virtual('addressString').get(function() {
+  return `${this.address.building} ${this.address.street}`.trim()});
 
+// this virtual grabs the most recent grade for a restaurant.
+postSchema.virtual('grade').get(function() {
+  const gradeObj = this.grades.sort((a, b) => {return b.date - a.date})[0] || {};
+  return gradeObj.grade;
+});
 
-function StorageException(message) {
-  this.message = message;
-  this.name = 'StorageException';
+// this is an *instance method* which will be available on all instances
+// of the model. This method will be used to return an object that only
+// exposes *some* of the fields we want from the underlying data
+postSchema.methods.serialize = function() {
+
+  return {
+    id: this._id,
+    name: this.name,
+    cuisine: this.cuisine,
+    borough: this.borough,
+    grade: this.grade,
+    address: this.addressString
+  };
 }
 
-const BlogPosts = {
-  create: function(title, content, author, publishDate) {
-    const post = {
-      id: uuid.v4(),
-      title: title,
-      content: content,
-      author: author,
-      publishDate: publishDate || Date.now()
-    };
-    this.posts.push(post);
-    return post;
-  },
-  get: function(id=null) {
-    // if id passed in, retrieve single post,
-    // otherwise send all posts.
-    if (id !== null) {
-      return this.posts.find(post => post.id === id);
-    }
-    // return posts sorted (descending) by
-    // publish date
-    return this.posts.sort(function(a, b) {
-      return b.publishDate - a.publishDate;
-    });
-  },
-  delete: function(id) {
-    const postIndex = this.posts.findIndex(
-      post => post.id === id);
-    if (postIndex > -1) {
-      this.posts.splice(postIndex, 1);
-    }
-  },
-  update: function(updatedPost) {
-    const {id} = updatedPost;
-    const postIndex = this.posts.findIndex(
-      post => post.id === updatedPost.id);
-    if (postIndex === -1) {
-      throw new StorageException(
-        `Can't update item \`${id}\` because doesn't exist.`);
-    }
-    this.posts[postIndex] = Object.assign(
-      this.posts[postIndex], updatedPost);
-    return this.posts[postIndex];
-  }
-};
+// note that all instance methods and virtual properties on our
+// schema must be defined *before* we make the call to `.model`.
+const Restaurant = mongoose.model('Restaurant', postSchema);
 
-function createBlogPostsModel() {
-  const storage = Object.create(BlogPosts);
-  storage.posts = [];
-  return storage;
-}
-
-
-module.exports = {BlogPosts: createBlogPostsModel()};
+module.exports = {Restaurant};
--- server.js
+++ server.js
@@ -1,47 +1,147 @@
 'use strict';
 
+const bodyParser = require('body-parser');
 const express = require('express');
-const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+const { PORT, DATABASE_URL } = require('./config');
+const { Post } = require('./models');
 
 const app = express();
-const blogRouter = require('./blogRouter');
+app.use(bodyParser.json());
 
-app.use(morgan('common'));
+app.get('/posts', (req, res) => {
+  const filters = {};
+  const queryableFields = ['cuisine', 'borough'];
+  queryableFields.forEach(field => {
+    if (req.query[field]) {
+      filters[field] = req.query[field];
+    }
+  });
+  Post
+    .find(filters)
+    .then(posts => res.json(
+      posts.map(post => post.serialize())
+    ))
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({message: 'Internal server error'});
+    });
+});
 
-app.use('/blog-posts', blogRouter);
+app.get('/posts/:id', (req, res) => {
+  Post
+    .findById(req.params.id)
+    .then(post => res.json(post.serialize()))
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ message: 'Internal server error' });
+    });
+});
+
+
+app.post('/posts', (req, res) => {
+
+  const requiredFields = ['name', 'borough', 'cuisine'];
+  for (let i = 0; i < requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`;
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+
+  Post
+    .create({
+      name: req.body.name,
+      borough: req.body.borough,
+      cuisine: req.body.cuisine,
+      grades: req.body.grades,
+      address: req.body.address
+    })
+    .then(post => res.status(201).json(post.serialize()))
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ message: 'Internal server error' });
+    });
+});
+
+
+app.put('/posts/:id', (req, res) => {
+  if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+    const message = (
+      `Request path id (${req.params.id}) and request body id ` +
+      `(${req.body.id}) must match`);
+    console.error(message);
+    return res.status(400).json({ message: message });
+  }
+
+  const toUpdate = {};
+  const updateableFields = ['name', 'borough', 'cuisine', 'address'];
+
+  updateableFields.forEach(field => {
+    if (field in req.body) {
+      toUpdate[field] = req.body[field];
+    }
+  });
+
+  Post
+    .findByIdAndUpdate(req.params.id, { $set: toUpdate })
+    .then(post => res.status(204).end())
+    .catch(err => res.status(500).json({ message: 'Internal server error' }));
+});
+
+app.delete('/posts/:id', (req, res) => {
+  Post
+    .findByIdAndRemove(req.params.id)
+    .then(post => res.status(204).end())
+    .catch(err => res.status(500).json({ message: 'Internal server error' }));
+});
+
+app.use('*', function (req, res) {
+  res.status(404).json({ message: 'Not Found' });
+});
 
 let server;
 
-// this function starts our server and returns a Promise.
-// In our test code, we need a way of asynchronously starting
-// our server, since we'll be dealing with promises there.
-function runServer() {
-  const port = process.env.PORT || 8080;
+function runServer(databaseUrl, port = PORT) {
+
   return new Promise((resolve, reject) => {
-    server = app.listen(port, () => {
-      console.log(`Your app is listening on port ${port}`);
-      resolve(server);
-    }).on('error', err => {
-      reject(err);
+    mongoose.connect(databaseUrl, err => {
+      if (err) {
+        return reject(err);
+      }
+      server = app.listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+        resolve();
+      })
+        .on('error', err => {
+          mongoose.disconnect();
+          reject(err);
+        });
     });
   });
 }
 
 function closeServer() {
-  return new Promise((resolve, reject) => {
-    console.log('Closing server');
-    server.close(err => {
-      if (err) {
-        reject(err);
-        return;
-      }
-      resolve();
+  return mongoose.disconnect().then(() => {
+    return new Promise((resolve, reject) => {
+      console.log('Closing server');
+      server.close(err => {
+        if (err) {
+          return reject(err);
+        }
+        resolve();
+      });
     });
   });
 }
 
 if (require.main === module) {
-  runServer().catch(err => console.error(err));
+  runServer(DATABASE_URL).catch(err => console.error(err));
 }
 
-module.exports = {app, runServer, closeServer};
+module.exports = { app, runServer, closeServer };
